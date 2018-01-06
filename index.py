c7467f593b6b8f6bd488ac6c93c82655dbdff728
Marko Jankovic <markoj@vast.com>
2018-01-07 00:37:29 +0100

    added logger, fixed issue with .lock file

--- index.py
+++ index.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 
 import stashy;
-import sys
-import os
-import re
-import subprocess
-import argparse
+import os;
+import re;
+import subprocess;
+import argparse;
+import logging;
+import sys;
+import signal;
+
+#logging.basicConfig(
+#    format='%(asctime)s %(name)s: %(levelname)s %(message)s (#pid %(process)d)',
+#    datefmt='%H:%M:%S',
+#    level=10
+#);
+
+logging.basicConfig(
+    format='%(message)s',
+    level=20
+);
+
+logger = logging.getLogger('stash-spy');
 
 class StashTrace:
     def __init__(self, args = None):
@@ -20,12 +35,19 @@ class StashTrace:
         parser.add_argument('-action', action='store', dest='action', help='Stash Action');
         parser.add_argument('-project', action='store', dest='project', help='Project Name');
 
+        signal.signal(signal.SIGINT, self.exitGracefully);
+        signal.signal(signal.SIGTERM, self.exitGracefully);
+        signal.signal(signal.SIGQUIT, self.exitGracefully);
+
         # Parsing CLI options
         argParams = parser.parse_args();
 
         # get {argParams.action} method
         return self.callMethod(argParams.action, argParams);
 
+
+    def exitGracefully(self,signum, frame):
+        logger.error('Gracefully Exit...');
 
     def callMethod(self, methodName, args):
         method = getattr(self, methodName);
@@ -36,14 +58,14 @@ class StashTrace:
     def clone(self, argParams):
         # stash url, username and password are mandatory
         if not (argParams.stashUrl and argParams.username and argParams.password):
-            print "Stashy login requires username, password and stash url"
+            logger.error('Stashy login requires username, password and stash url')
             exit();
 
         # Connect to stash
         stash = stashy.connect(argParams.stashUrl, argParams.username, argParams.password);
 
         # if dest is not defined save in root
-        dest = "../" + argParams.dest if argParams.dest else "./";
+        dest = '../' + argParams.dest if argParams.dest else './';
 
         if argParams.project:
             projectList = stash.projects[argParams.project].repos.list();
@@ -54,97 +76,107 @@ class StashTrace:
             allProjects = stash.projects.list();
 
             for project in allProjects:
-                projectList = stash.projects[project[unicode("key")]].repos.list();
+                projectList = stash.projects[project[unicode('key')]].repos.list();
 
-                self.cloneAllRepos(dest, project[unicode("key")], projectList);
+                self.cloneAllRepos(dest, project[unicode('key')], projectList);
 
 
     def cloneAllRepos(self, dest, projectName , projectList):
         for repo in projectList:
 
             # repo name
-            repoName = repo[unicode("name")];
+            repoName = repo[unicode('name')];
 
-            projectPath = os.path.join(projectName, repoName);
+            # replace space with dash and lowercase
+            projectPath = os.path.join(projectName, repoName.replace(' ', '-').lower());
 
-            # absolut dir path
-            gitDir = "'" + os.path.abspath(os.path.join(dest, projectPath)) + "'";
+            # absolute dir path
+            gitDir = os.path.abspath(os.path.join(dest, projectPath));
 
             # clone repo if dir does not exist
             if (os.path.exists(gitDir) == False):
 
                 # repo clone url
-                cloneUrl = repo[unicode("cloneUrl")];
+                cloneUrl = repo[unicode('cloneUrl')];
 
                 # clone repo in root/projectName/repoName
-                self.systemCall("git clone " + cloneUrl + " " + gitDir);
+                self.systemCall('git clone ' + cloneUrl + ' ' + gitDir);
 
                 # check if dir has .git
-                isGit = self.systemCall("git rev-parse --is-inside-work-tree", gitDir);
+                isGit = self.systemCall('git rev-parse --is-inside-work-tree', gitDir);
 
                 if isGit != False:
-                    print "#####", projectPath, "#####"
+                    logger.info('\n');
+                    logger.info('#### %s', projectPath);
                     self.checkRepo(gitDir)
-                    print "-----------------------------------------\n"
-
             else:
-                print "#####", projectPath, "#####"
-                self.checkRepo(gitDir, True)
-                print "-----------------------------------------\n"
+                logger.info('\n');
+                logger.info('#### %s', projectPath);
+                self.checkRepo(gitDir, True);
 
 
     def systemCall(self, command, cwd = None):
-        # --quiet
+        cmd = False;
 
         try:
             if cwd:
-                output = subprocess.check_output("cd " + cwd + " && " + command, shell=True)
+                output = subprocess.check_output('cd ' + cwd + ' && ' + command, shell=True);
             else:
-                output = subprocess.check_output(command, shell=True)
+                output = subprocess.check_output(command, shell=True);
 
             if len(output) > 0:
-                return output
+                return output;
 
             else:
-                return False
+                return False;
 
         except:
-            return False
+            return False;
 
 
     def isBranchExist(self, branchName, gitDir):
-        return self.systemCall("git branch | grep -w " + branchName, gitDir)
+        return self.systemCall('git branch | grep -w ' + branchName, gitDir)
 
 
     def checkRepo (self, gitDir, exist = None):
         # deleting the refs to the branches
         # that don't exist on the remote
-        self.systemCall("git fetch --prune", gitDir);
+        logger.debug("... fetch prune ....")
+        self.systemCall('git fetch --prune', gitDir);
+
+        # fix for "fatal: Unable to create '/*/.git/index.lock': File exists."
+        self.systemCall('rm -f ./.git/index.lock', gitDir);
 
         # get list of all remotes
-        grepBranhes = self.systemCall("git branch --all | grep origin | grep -v master | grep -v HEAD", gitDir)
+        logger.debug("... branch grep ....")
+        grepBranhes = self.systemCall('git branch --all | grep origin | grep -v master | grep -v HEAD', gitDir)
 
         if grepBranhes != False:
-
             # branch list
-            allBranches = re.sub("\s+", " ", grepBranhes).strip().split(" ");
+            allBranches = re.sub('\s+', ' ', grepBranhes).strip().split(' ');
 
             # git checkout for each branch and git pull
             for branch in allBranches:
                 # branch name
-                branchName = branch.replace("remotes/origin/", "");
+                branchName = branch.replace('remotes/origin/', '');
 
                 # if branch exists do --track
                 if self.isBranchExist(branchName, gitDir) is False:
-                    self.systemCall("git checkout --track origin/" + branchName, gitDir);
+                    logger.debug("... checkout origin ....")
+                    # create local branch {branchName}
+                    self.systemCall('git checkout --track origin/' + branchName, gitDir);
+
+                    logger.debug("... pull all ....")
+                    self.systemCall('git pull --all', gitDir);
                 else:
                     # checkout branch and discard local changes
-                    self.systemCall("git checkout -f " + branchName, gitDir);
+                    self.systemCall('git checkout -f ' + branchName, gitDir);
 
-                # --ff-only Refuse to merge and exit with a non-zero status unless the current
+                # --ff-only
+                # Refuse to merge and exit with a non-zero status unless the current
                 # HEAD is already up-to-date or the merge can be resolved as a fast-forward
                 if exist == True:
-                    self.systemCall("git pull --ff-only", gitDir);
-
+                    logger.debug("... pull ff-only ....")
+                    self.systemCall('git pull --ff-only', gitDir);
 
 StashTrace()
